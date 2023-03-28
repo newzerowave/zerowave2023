3fd2a205f9e2cad9a00a6f1fab59e6c66bcf8c3e
eeuih <neuih2@gmail.com>
2023-03-28 17:59:19 +0900

    new setting

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+                  Learn React
+           </header>
+    </div>
+  );
+}
+
+export default App;
